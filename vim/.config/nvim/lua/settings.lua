fb668e2d94ba85b652399722a7cfe2c94ad89d23
moronvv <moronvv@gmail.com>
2023-02-10 20:48:03 +0300

    [vim] move plugin manager to init.vim

---
+++ vim/.config/nvim/lua/settings.lua
@@ -0,0 +1,34 @@
+-- relative numbers
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+-- enable break indent
+vim.opt.breakindent = true
+vim.opt.linebreak = true
+vim.opt.smartindent = true
+
+-- no backups
+vim.opt.backup = false
+vim.opt.writebackup = false
+vim.opt.swapfile = false
+
+-- save undo history.
+vim.opt.undofile = true
+
+-- case insensitive searching unless /C or capital in search.
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- decrease update time.
+vim.opt.updatetime = 250
+vim.opt.signcolumn = "yes"
+
+-- split windows
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- lazy redraw when macro working
+vim.opt.lazyredraw = true
+
+-- hide mode below statusline
+vim.opt.showmode = false
