b7a02e113389211b605c703250ff110893f76ab2
moronvv <moronvv@gmail.com>
2023-02-13 17:14:12 +0300

    [vim] remove vinegar plugin, add helper plugin for gx

--- vim/.config/nvim/lua/plugins/misc.lua
+++ vim/.config/nvim/lua/plugins/misc.lua
@@ -3,11 +3,13 @@ return {
   "tpope/vim-unimpaired", -- move operations with [ and ]
   "tpope/vim-repeat", -- helper for . repeat operations
   "tpope/vim-abolish", -- smart sabstitution
-  "tpope/vim-vinegar", -- compfy settings for netrw
   "tpope/vim-sleuth", -- 'shiftwidth' and 'expandtab' automatical adjustment
   "tpope/vim-obsession", -- session saver
   "tpope/vim-eunuch", -- helper unix commands for files
-  "nelstrom/vim-visual-star-search", -- star serch on visual selection
-  "romainl/vim-cool", -- disables highlighting after search
+
   "christoomey/vim-tmux-navigator", -- tmux
+
+  "nelstrom/vim-visual-star-search", -- star serch on visual selection
+  { "felipec/vim-sanegx", keys = "gx" }, -- fix links opening by gx
+  "romainl/vim-cool", -- disables highlighting after search
 }
